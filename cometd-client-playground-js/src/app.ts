60d54583602ea5faa92f18f538d34094ffdc6811
Aleksandr Sinotov <bratsinot@gmail.com>
2022-11-21 15:05:40 +0100

    Add dumps.

---
+++ cometd-client-playground-js/src/app.ts
@@ -0,0 +1,31 @@
+import {adapt} from 'cometd-nodejs-client';
+import {CometD, Message} from 'cometd';
+
+const json = function (data: any): string {
+    //return JSON.stringify(data, null, 2);
+    return JSON.stringify(data);
+};
+
+adapt();
+const cometd = new CometD();
+
+cometd.configure({
+    url: 'http://[::1]:1025/notifications',
+});
+
+cometd.handshake(function (message: Message) {
+    console.log("Try to do handshake.")
+    if (message.successful) {
+        console.log("Got handshake: `", json(message), "`.");
+
+        // Subscribe to receive messages from the server.
+        cometd.subscribe('/topic', function (message: Message) {
+            console.log("Got subscribe message: `", json(message), "`.");
+
+            cometd.disconnect(function (message: Message) {
+                console.log("Got disconnect message: `", json(message), "`.");
+            });
+            // Use dataFromServer.
+        });
+    }
+});
---
+++ cometd-server-playground-js/src/app.ts
@@ -0,0 +1,78 @@
+import * as cometd from 'cometd-nodejs-server';
+import * as express from 'express';
+import {Server as HttpServer} from 'http';
+import * as http from 'http';
+
+const json = function (data: any): string {
+    //return JSON.stringify(data, null, 2);
+    return JSON.stringify(data);
+};
+
+const createServer = (app): HttpServer => {
+    app.locals.protocol = 'http';
+
+    let server = http.createServer(app);
+    server.keepAliveTimeout = 70000;
+    server.headersTimeout = 120000;
+
+    return server;
+};
+
+const app = express();
+
+const cometdServer = cometd.createCometDServer({
+    timeout: 20000,
+    maxInterval: 60000,
+    duplicateMetaConnectHttpResponseCode: 409 //GAPI-18907
+});
+
+const channel = cometdServer.createServerChannel('/topic');
+channel.addListener('message', function (session, channel, message, callback) {
+    /*console.log("Got message: ", {
+        "session": session,
+        "channel": channel,
+        "message": message,
+        "callback": callback,
+    });*/
+
+    // Invoke the callback to signal that handling is complete.
+    callback();
+});
+
+cometdServer.addListener('sessionAdded', (cometConnection, msg) => {
+    console.log("sessionAdded, (cometConnection: \"" + json(cometConnection), "\", msg: \"", msg, "\"");
+});
+cometdServer.addListener('sessionRemoved', (cometConnection, timeout) => {
+    console.log("sessionRemoved, (cometConnection: \"" + json(cometConnection), "\", timeout: \"", timeout, "\"");
+    //
+});
+
+app.use('/notifications', cometdServer.handle);
+app.set('port', 1025);
+
+let server = createServer(app);
+let port = app.get('port');
+
+server.listen(port, function (): void {
+    console.log(`Server running on port ${port}.`);
+
+    setTimeout(function () {
+            channel.publish("Vasya", {
+                "type": "from_server",
+                "message": "Some json"
+            });
+        },
+        5000);
+    //loop();
+});
+
+/*function loop() {
+    setTimeout(function () {
+        console.log("Publish");
+        channel.publish("Vasya", {
+            "type": "from_server",
+            "foo": "bar"
+        });
+        loop();
+    }, 1000)
+}*/
