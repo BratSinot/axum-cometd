11b8809a374aed510605c70685ff5ae255d9f9ca
Aleksander <bratsinot@gmail.com>
2022-11-28 12:09:11 +0200

    Example now send messages to two topics. (#18)

--- examples/server/src/main.rs
+++ examples/server/src/main.rs
@@ -1,20 +1,33 @@
-use axum_cometd::{LongPoolingServiceContextBuilder, RouterBuilder};
-use std::{borrow::Cow, error::Error, fmt::Debug, time::Duration};
+use axum_cometd::{LongPoolingServiceContext, LongPoolingServiceContextBuilder, RouterBuilder};
+use rand::{distributions::Uniform, rngs::StdRng, Rng, SeedableRng};
+use std::{
+    fmt::Debug,
+    sync::Arc,
+    time::{Duration, SystemTime, UNIX_EPOCH},
+};
 
 #[derive(Debug, Clone, serde::Serialize)]
-struct Data<'a> {
-    msg: Cow<'a, str>,
-    r#bool: bool,
-    num: u64,
+struct Data {
+    channel: &'static str,
+    msg: String,
+    timestamp: u64,
+}
+
+#[inline]
+fn timestamp() -> u64 {
+    SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs()
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() {
     tracing_subscriber::fmt()
         .with_env_filter("axum_cometd=trace")
         .init();
 
-    let addr = "[::0]:1025".parse()?;
+    let addr = "[::0]:1025".parse().unwrap();
 
     let context = LongPoolingServiceContextBuilder::new()
         .timeout_ms(1000)
@@ -30,17 +43,31 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     tokio::task::spawn(axum::Server::bind(&addr).serve(app.into_make_service()));
 
-    loop {
-        context
-            .send(
-                "/topic",
-                Data {
-                    msg: "Hello World!!!".into(),
-                    r#bool: true,
-                    num: u64::MAX,
-                },
-            )
-            .await?;
-        tokio::time::sleep(Duration::from_millis(1000)).await;
-    }
+    spawn_topic(context.clone(), "/topic0");
+    spawn_topic(context, "/topic1");
+
+    tokio::time::sleep(Duration::from_secs(366 * 24 * 60)).await;
+}
+
+fn spawn_topic(context: Arc<LongPoolingServiceContext<Data>>, channel: &'static str) {
+    tokio::task::spawn(async move {
+        let mut rng: StdRng = SeedableRng::from_entropy();
+        let distribution = Uniform::new(1000, 2000);
+
+        loop {
+            context
+                .send(
+                    channel,
+                    Data {
+                        channel,
+                        msg: format!("Hello from {channel}"),
+                        timestamp: timestamp(),
+                    },
+                )
+                .await
+                .unwrap();
+
+            tokio::time::sleep(Duration::from_millis(rng.sample(distribution))).await;
+        }
+    });
 }
