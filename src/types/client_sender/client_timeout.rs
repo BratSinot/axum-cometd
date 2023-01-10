d0b4d3866adb72b93eef4ec182e73083fb6eb16e
Aleksander <bratsinot@gmail.com>
2023-01-10 17:39:34 +0100

    Use Notify::notify_one instead of CancellationToken. (#65)

--- src/types/client_sender/client_timeout.rs
+++ src/types/client_sender/client_timeout.rs
@@ -2,20 +2,19 @@ use crate::types::ClientId;
 use crate::LongPollingServiceContext;
 use std::{sync::Arc, time::Duration};
 use tokio::{select, sync::Notify, time};
-use tokio_util::sync::CancellationToken;
 
 pub(super) fn spawn(
     context: Arc<LongPollingServiceContext>,
     client_id: ClientId,
     timeout: Duration,
-    stop_signal: CancellationToken,
+    stop_signal: Arc<Notify>,
     start_timeout: Arc<Notify>,
     cancel_timeout: Arc<Notify>,
 ) {
     tokio::task::spawn(async move {
         loop {
             select! {
-                _ = stop_signal.cancelled() => break,
+                _ = stop_signal.notified() => break,
                 _ = time::sleep(timeout) => {
                     tracing::info!(
                         client_id = %client_id,
@@ -35,12 +34,9 @@ pub(super) fn spawn(
 }
 
 #[inline]
-async fn wait_until_client_disconnect(
-    stop_signal: &CancellationToken,
-    start_timeout: &Notify,
-) -> bool {
+async fn wait_until_client_disconnect(stop_signal: &Notify, start_timeout: &Notify) -> bool {
     select! {
         _ = start_timeout.notified() => false,
-        _ = stop_signal.cancelled() => true,
+        _ = stop_signal.notified() => true,
     }
 }
