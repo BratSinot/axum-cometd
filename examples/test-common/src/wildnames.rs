9d8c5391dfdf2a258d4a5b33930d23afddc8ac9a
Aleksandr Sinotov <bratsinot@gmail.com>
2023-01-11 13:16:45 +0100

    Get rid of once_cell in tests.

--- examples/test-common/src/wildnames.rs
+++ examples/test-common/src/wildnames.rs
@@ -1,113 +1,94 @@
-use once_cell::sync::Lazy;
-
-#[allow(clippy::type_complexity)]
-pub static TEST_WILDNAMES: Lazy<[(&str, Option<Vec<String>>); 42]> = Lazy::new(|| {
-    [
-        // 0 nest level
-        (r#""#, None),
-        (r#"/*"#, Some(vec![])),
-        (r#"/**"#, Some(vec![])),
-        // 1 nest level
-        ("/first1", Some(vec!["/*".to_owned(), "/**".to_owned()])),
-        ("/first1*", None),
-        ("/first1**", None),
-        (
-            "/first1/",
-            Some(vec![
-                "/first1/*".to_owned(),
-                "/first1/**".to_owned(),
-                "/**".to_owned(),
-            ]),
-        ),
-        ("/first1/*", Some(vec![])),
-        ("/first1/ *", None),
-        ("/first1/**", Some(vec![])),
-        ("/first1/ **", None),
-        // 2 nest levels
-        (
-            "/first1/second2",
-            Some(vec![
-                "/first1/*".to_owned(),
-                "/first1/**".to_owned(),
-                "/**".to_owned(),
-            ]),
-        ),
-        ("/first1/second2*", None),
-        ("/first1/second2**", None),
-        (
-            "/first1/second2/",
-            Some(vec![
-                "/first1/second2/*".to_owned(),
-                "/first1/second2/**".to_owned(),
-                "/first1/**".to_owned(),
-                "/**".to_owned(),
-            ]),
-        ),
-        ("/first1/second2/*", Some(vec![])),
-        ("/first1/second2/ *", None),
-        ("/first1/second2/**", Some(vec![])),
-        ("/first1/second2/ **", None),
-        // 3 nest levels
-        (
-            "/first1/second2/third3",
-            Some(vec![
-                "/first1/second2/*".to_owned(),
-                "/first1/second2/**".to_owned(),
-                "/first1/**".to_owned(),
-                "/**".to_owned(),
-            ]),
-        ),
-        ("/first1/second2/third3*", None),
-        ("/first1/second2/third3**", None),
-        (
-            "/first1/second2/third3/",
-            Some(vec![
-                "/first1/second2/third3/*".to_owned(),
-                "/first1/second2/third3/**".to_owned(),
-                "/first1/second2/**".to_owned(),
-                "/first1/**".to_owned(),
-                "/**".to_owned(),
-            ]),
-        ),
-        ("/first1/second2/third3/*", Some(vec![])),
-        ("/first1/second2/third3/ *", None),
-        ("/first1/second2/third3/**", Some(vec![])),
-        ("/first1/second2/third3/ **", None),
-        // 4 nest levels + special chars
-        (
-            "/first1/second2/third3/-_!~()$@",
-            Some(vec![
-                "/first1/second2/third3/*".to_owned(),
-                "/first1/second2/third3/**".to_owned(),
-                "/first1/second2/**".to_owned(),
-                "/first1/**".to_owned(),
-                "/**".to_owned(),
-            ]),
-        ),
-        ("/first1/second2/third3/-_!~()$@*", None),
-        ("/first1/second2/third3/-_!~()$@**", None),
-        (
-            "/first1/second2/third3/-_!~()$@/",
-            Some(vec![
-                "/first1/second2/third3/-_!~()$@/*".to_owned(),
-                "/first1/second2/third3/-_!~()$@/**".to_owned(),
-                "/first1/second2/third3/**".to_owned(),
-                "/first1/second2/**".to_owned(),
-                "/first1/**".to_owned(),
-                "/**".to_owned(),
-            ]),
-        ),
-        ("/first1/second2/third3/-_!~()$@/*", Some(vec![])),
-        ("/first1/second2/third3/-_!~()$@/ *", None),
-        ("/first1/second2/third3/-_!~()$@/**", Some(vec![])),
-        ("/first1/second2/third3/-_!~()$@/ **", None),
-        // use wild characters in the middle
-        ("/first1/*/third3", None),
-        ("/first1/*/third3/", None),
-        ("/first1/*/third3/*", None),
-        ("/first1/*/third3/**", None),
-        ("/first1/second2/**/", None),
-        ("/first1/second2/**/*", None),
-        ("/first1/second2/**/**", None),
-    ]
-});
+pub const TEST_WILDNAMES: [(&str, Option<&[&str]>); 42] = [
+    // 0 nest level
+    (r#""#, None),
+    (r#"/*"#, Some(&[])),
+    (r#"/**"#, Some(&[])),
+    // 1 nest level
+    ("/first1", Some(&["/*", "/**"])),
+    ("/first1*", None),
+    ("/first1**", None),
+    ("/first1/", Some(&["/first1/*", "/first1/**", "/**"])),
+    ("/first1/*", Some(&[])),
+    ("/first1/ *", None),
+    ("/first1/**", Some(&[])),
+    ("/first1/ **", None),
+    // 2 nest levels
+    ("/first1/second2", Some(&["/first1/*", "/first1/**", "/**"])),
+    ("/first1/second2*", None),
+    ("/first1/second2**", None),
+    (
+        "/first1/second2/",
+        Some(&[
+            "/first1/second2/*",
+            "/first1/second2/**",
+            "/first1/**",
+            "/**",
+        ]),
+    ),
+    ("/first1/second2/*", Some(&[])),
+    ("/first1/second2/ *", None),
+    ("/first1/second2/**", Some(&[])),
+    ("/first1/second2/ **", None),
+    // 3 nest levels
+    (
+        "/first1/second2/third3",
+        Some(&[
+            "/first1/second2/*",
+            "/first1/second2/**",
+            "/first1/**",
+            "/**",
+        ]),
+    ),
+    ("/first1/second2/third3*", None),
+    ("/first1/second2/third3**", None),
+    (
+        "/first1/second2/third3/",
+        Some(&[
+            "/first1/second2/third3/*",
+            "/first1/second2/third3/**",
+            "/first1/second2/**",
+            "/first1/**",
+            "/**",
+        ]),
+    ),
+    ("/first1/second2/third3/*", Some(&[])),
+    ("/first1/second2/third3/ *", None),
+    ("/first1/second2/third3/**", Some(&[])),
+    ("/first1/second2/third3/ **", None),
+    // 4 nest levels + special chars
+    (
+        "/first1/second2/third3/-_!~()$@",
+        Some(&[
+            "/first1/second2/third3/*",
+            "/first1/second2/third3/**",
+            "/first1/second2/**",
+            "/first1/**",
+            "/**",
+        ]),
+    ),
+    ("/first1/second2/third3/-_!~()$@*", None),
+    ("/first1/second2/third3/-_!~()$@**", None),
+    (
+        "/first1/second2/third3/-_!~()$@/",
+        Some(&[
+            "/first1/second2/third3/-_!~()$@/*",
+            "/first1/second2/third3/-_!~()$@/**",
+            "/first1/second2/third3/**",
+            "/first1/second2/**",
+            "/first1/**",
+            "/**",
+        ]),
+    ),
+    ("/first1/second2/third3/-_!~()$@/*", Some(&[])),
+    ("/first1/second2/third3/-_!~()$@/ *", None),
+    ("/first1/second2/third3/-_!~()$@/**", Some(&[])),
+    ("/first1/second2/third3/-_!~()$@/ **", None),
+    // use wild characters in the middle
+    ("/first1/*/third3", None),
+    ("/first1/*/third3/", None),
+    ("/first1/*/third3/*", None),
+    ("/first1/*/third3/**", None),
+    ("/first1/second2/**/", None),
+    ("/first1/second2/**/*", None),
+    ("/first1/second2/**/**", None),
+];
