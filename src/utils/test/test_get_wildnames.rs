c954426fab6db6d4b2db4471775468a2d59a40c1
Aleksander <bratsinot@gmail.com>
2023-01-11 13:23:45 +0100

    Get rid of once_cell in tests. (#67)

--- src/utils/test/test_get_wildnames.rs
+++ src/utils/test/test_get_wildnames.rs
@@ -6,12 +6,14 @@ fn test_get_wildnames() {
     let validator = ChannelNameValidator::default();
 
     for (channel, result) in TEST_WILDNAMES.iter() {
-        assert_eq!(
-            validator
-                .validate_subscribe_channel_name(channel)
-                .then(|| get_wild_names(channel)),
-            *result,
-            "{channel}"
-        );
+        let ret = validator
+            .validate_subscribe_channel_name(channel)
+            .then(|| get_wild_names(channel));
+
+        match (ret, *result) {
+            (Some(ret), Some(result)) => assert_eq!(ret, result, "{channel}"),
+            (Some(ret), None) => assert_eq!(Some(ret), None, "{channel}"),
+            (None, result) => assert_eq!(None, result, "{channel}"),
+        }
     }
 }
