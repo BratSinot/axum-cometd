814943f94c4b838f3fba3e2670eb133e78bea532
Aleksandr Sinotov <bratsinot@gmail.com>
2023-01-12 19:51:35 +0100

    Use VecDeque::push_front instead of Vec::insert(0 in get_wild_names.

--- src/utils/wildnames.rs
+++ src/utils/wildnames.rs
@@ -1,24 +1,26 @@
+use std::collections::VecDeque;
+
 // TODO: ret.insert(0, <...>) => ret.push(<...>)
-pub(crate) fn get_wild_names(name: &str) -> Vec<String> {
+pub(crate) fn get_wild_names(name: &str) -> VecDeque<String> {
     let mut segments = name.split('/');
     let last_segment = segments.next_back();
 
     if !name.is_empty() && !matches!(last_segment, Some("*") | Some("**")) {
         let len = count_wildnames(name);
 
-        let mut ret = Vec::with_capacity(len);
+        let mut ret = VecDeque::with_capacity(len);
         let mut wild = String::with_capacity(name.len());
 
         for segment in segments {
             wild.push_str(segment);
             wild.push('/');
-            ret.insert(0, format!("{wild}**"));
+            ret.push_front(format!("{wild}**"));
         }
-        ret.insert(0, format!("{wild}*"));
+        ret.push_front(format!("{wild}*"));
 
         ret
     } else {
-        Vec::with_capacity(0)
+        VecDeque::with_capacity(0)
     }
 }
 
--- src/utils/wildnames_cache.rs
+++ src/utils/wildnames_cache.rs
@@ -1,16 +1,17 @@
 use crate::{types::ChannelId, utils::get_wild_names};
 use ahash::{AHashMap, AHashSet};
+use std::collections::VecDeque;
 use std::sync::Arc;
 use tokio::sync::RwLock;
 
 #[derive(Debug, Default)]
 pub(crate) struct WildNamesCache {
     // TODO: Replace on Arc<[String]>
-    cache: RwLock<AHashMap<ChannelId, Arc<Vec<String>>>>,
+    cache: RwLock<AHashMap<ChannelId, Arc<VecDeque<String>>>>,
 }
 
 impl WildNamesCache {
-    pub(crate) async fn fetch_wildnames(&self, name: &str) -> Arc<Vec<ChannelId>> {
+    pub(crate) async fn fetch_wildnames(&self, name: &str) -> Arc<VecDeque<ChannelId>> {
         let read_guard = self.cache.read().await;
         if let Some(wildnames) = read_guard.get(name) {
             wildnames.clone()
